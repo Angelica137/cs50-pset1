51d0e4e41c4e82da0f9f16d8953df7d8c756613f
Angelica <Angelica137@users.noreply.github.com>
2020-07-03 13:14:03 +0100

    complete answer, all works

--- mario.c
+++ mario.c
@@ -1,114 +1,27 @@
 #include <stdio.h>
-#include <string.h>
 #include <cs50.h>
-
-/*mario pyramids are built using user inpput*/
 
 int main()
 {
-    int height;
-    do
-    {
-       height = get_int("Height: ");
-    }
-    while(height < 1 || height > 8);
-
-    printf("Height: %d\n", height);
-    
-    // Declaring character pointers
-    char *ptr1 = "#";
-    char *ptr2 = " ";
-    
-    // Declaring array for gap
-    char* gap[2] = {" ", " ",};
-    
-    // Declaring array of length hight
-    char* n[height];
-	
-	// Assigning values to array index
-	int i = 0;
-	for (i = 0; i < height; i++)
-	{
-    	n[i] = ptr1;
-	}
-	return n;
-
-    char *strcat(char *dest, const char *src);
-    
-    char str[1024] = "Hello World";
-    char tmp = '\0';
-    size_t len = strlen(str);
-    printf("%s\n", n[i]);
-    snprintf(str + len, sizeof str - len, "%c", tmp);
-    printf("%s\n", str);
-
-}
-
-
-
-
-
-
-#include <stdio.h>
-#include <cs50.h>
-
-void hash_loop(int rowNumber);
-
-int get_valid_height(void);
-
-int main(void)
-{
-    int height = get_valid_height();
-
-    for (int i = 0; i < height; i++)
-    {
-        int spaces = height - i - 1;
-        printf("%*s", spaces, "");
-        hash_loop(i);
-        printf("  ");
-        hash_loop(i);
-        printf("\n");
-    }
-
-}
-/*this function creates the hashes*/
-void hash_loop(int rowNumber)
-{
-    for (int j = 0; j <= rowNumber; j++)
-    {
-        printf("#");
-    }
-}
-
-/* this function gets the height*/
-int get_valid_height(void)
-{
-    int height;
-    do
-    {
-        height = get_int("Height: ");
-    }
-    while (height < 1 || height > 8);
-    return height;
-}
-
-
-// 3rd attempt
-
-/*main is a function, int is its returnt type. I am declaring a function, you might need to return 0*/
-int main() 
-{
-	/* declare your vars8 */
+	/* declare your vars */
 	int height,row,space,symbol;
-	/*get user input*/
+	char *gap = "  ";
+	//get user input
     do
     {
        height = get_int("Height: ");
     }
     while(height < 1 || height > 8);
 
-
+		//print pyramid
+		for (row = 1; row <= height; row++) {
+		space = height - row;
+		printf("%*s", space, "");
+		symbol = row;
+		printf("%.*s", symbol, "##########");
+		printf("%s", gap);
+		printf("%.*s", symbol, "##########");
+		printf("\n");
+		}
 		return 0;
-
-
 }
