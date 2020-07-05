32f7ef72094f2a587a8d1a9bec6352d0d9ed48a4
Angelica <Angelica137@users.noreply.github.com>
2020-07-05 13:11:42 +0100

    I think thes works, still need to test

--- credit.c
+++ credit.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main()
 {
 	  //declare your var
-	  long user_input;
+	  long userInput;
 	
 	  //get user input
-    user_input = get_long("Number: ");
+    userInput = get_long("Number: ");
     
 		// checksum function
-		long number = user_input;
+		long number = userInput;
 		int sum = 0;
 		int count = 0;
+		long divisor = 10;
+		char result[11];
     
 		// sum alternate digits from last digit
 		while(number > 0)
@@ -24,7 +27,7 @@ int main()
 		printf("%d\n", sum);
     
 		// sum alternate digits from second to last digit and time by 2
-		number = user_input / 10;
+		number = userInput / 10;
 		while(number > 0)
 		{
 			int lastDigit = number % 10;
@@ -40,14 +43,47 @@ int main()
 		// get the first digits of the number
 		// get the length of the number, muiltiply by 10 to know 
 		// how many zeroes to move to the left to get divisor
-    number = user_input;
-		printf("%d\n", number);
+    number = userInput;
 		while(number != 0)
 		{
 			number = number / 10;
 			count++;
 		}
-    printf("%i\n", count);
+    // increase the divisor to count - 2
+		for (int i = 0; i < count - 2; i++)
+		{
+			divisor = divisor * 10;
+		}
+
+    int firstDigit = userInput / divisor;
+		int firstTwoDigits = userInput / (divisor / 10);
+    
+		//check if the number is valid
+		if (sum % 10 == 0)
+		{
+       if (firstDigit == 4 && (count == 13 || count == 16))
+			 {
+         strcpy(result, "VISA");
+			 }
+			 else if ((firstTwoDigits == 34 || firstTwoDigits == 37) && count == 15)
+			 {
+         strcpy(result, "AMEX");
+			 }
+			 else if ((50 < firstTwoDigits && firstTwoDigits < 56) && count == 16)
+			 {
+				 strcpy(result, "MASTERCARD");
+			 }
+			 else
+			 {
+				strcpy(result, "Invalid");
+			 }
+		}
+		else 
+		{
+			strcpy(result, "Invalid");
+		}
+
+		printf("%s\n", result);
 
 		return 0;
 }
